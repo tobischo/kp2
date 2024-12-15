b5d0994619aaa5306278686c4f5ae7acfc3d3baf
Tobias Schoknecht <tobias.schoknecht@paysafe.com>
2024-12-15 11:23:49 +0100

    Update linter config

--- generate.go
+++ generate.go
@@ -83,7 +83,7 @@ func generateCmd(_ *cobra.Command, _ []string) error {
 func generatePassword(baseBytes string, length int) (string, error) {
 	var password string
 
-	for i := 0; i < length; i++ {
+	for range length {
 		val, err := rand.Int(rand.Reader, big.NewInt(int64(len(baseBytes))))
 		if err != nil {
 			return "", err
